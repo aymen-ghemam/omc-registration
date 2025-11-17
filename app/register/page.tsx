7b4af3e845a34ea846fb68c648cf2babb6cc7419
DjeghlafLydia <djeghlaf.habiba@gmail.com>
2025-11-17 07:49:46 +0100

    ADD

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/header";
+
+export default function Home() {
+  return (
+    <div className="page-background">
+      <Header />
+      <div className="text-6xl">
+        holaaaaa hii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hihii <br/>hi
+      </div>
+    </div>
+  );
+}
