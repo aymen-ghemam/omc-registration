15cf2614ef408e378f825ee87b48ca0d5aa92369
aymen ghemam <gh.aymen39@gmail.com>
2025-11-23 12:12:40 +0100

    Update registration deadline to Nov 23 2025

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 // Registration deadline: November 23rd, 2025 at 8:00 PM Algeria Time (CET - UTC+1)
-export const REGISTRATION_DEADLINE = new Date("2025-11-22T20:00:00+01:00");
+export const REGISTRATION_DEADLINE = new Date("2025-11-23T20:00:00+01:00");
 
 // Discord community invite link
 export const DISCORD_INVITE_URL = "https://discord.gg/zYNew6HW9y";
